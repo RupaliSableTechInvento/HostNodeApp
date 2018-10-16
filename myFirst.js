c209ab042ded773d050aeeb9252767f503043329
RupaliSableTechInvento <37444188+RupaliSableTechInvento@users.noreply.github.com>
2018-10-16 18:44:35 +0530

    procfile added

--- myFirst.js
+++ myFirst.js
@@ -1,5 +1,6 @@
 var http = require('http');
+const PORT = process.env.PORT||'8080';
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
-}).listen(process.env.PORT ||8080);
+}).listen(PORT);
